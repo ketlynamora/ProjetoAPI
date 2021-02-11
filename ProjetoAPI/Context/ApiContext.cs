9d4d1df353d05aa29a80b853ce2605513e664ca2
Ketlyn Amora <ketypricila70@gmail.com>
2021-02-11 14:15:17 -0300

    commit inicial

---
+++ ProjetoAPI/Context/ApiContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ProjetoAPI.Models
+{
+    public partial class ApiContext : DbContext
+    {
+        
+        public ApiContext(DbContextOptions<ApiContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Usuarios> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuarios>(entity =>
+            {
+                entity.HasKey(e => e.IdUsuario);
+
+                entity.ToTable("usuarios");
+
+                entity.HasIndex(e => e.Email)
+                    .HasName("email")
+                    .IsUnique();
+
+                entity.Property(e => e.IdUsuario).HasColumnName("idUsuario");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasColumnName("email")
+                    .HasColumnType("varchar(150)");
+
+                entity.Property(e => e.Nome)
+                    .IsRequired()
+                    .HasColumnName("nome")
+                    .HasColumnType("varchar(80)");
+
+                entity.Property(e => e.Senha)
+                    .IsRequired()
+                    .HasColumnName("senha")
+                    .HasColumnType("varchar(80)");
+            });
+        }
+    }
+}
---
+++ ProjetoAPI/Controllers/SegurancaController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using ProjetoAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SegurancaController : ControllerBase
+    {
+        private IConfiguration _config;
+
+        public SegurancaController(IConfiguration Configuration)
+        {
+            _config = Configuration;
+        }
+
+        private bool ValidarUsuario(TokenLogin login)
+        {
+            if (login.Usuario == "Ketlyn" && login.Senha == "proxys1")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private string GerarToken()
+        {
+            var issuer = _config["Jwt:Issuer"];
+            var audicence = _config["Jwt:Audience"];
+            var expiry = DateTime.Now.AddMinutes(120);
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken
+            (
+                issuer : issuer, 
+                audience: audicence,
+                expires: expiry,
+                signingCredentials: credentials
+            );
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var stringToken = tokenHandler.WriteToken(token);
+
+            return stringToken;
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public IActionResult Login ([FromBody]TokenLogin login)
+        {
+            bool resultado = ValidarUsuario(login);
+            if (resultado)
+            {
+                var tokenString = GerarToken();
+                return Ok(new TokenRetorno { Token = tokenString, DataTokenGerado = DateTime.Now });
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+    }
+}
